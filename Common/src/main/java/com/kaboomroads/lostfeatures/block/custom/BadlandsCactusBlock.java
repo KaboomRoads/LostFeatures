2985bcc1c3c8b22d95e922602c054ecc4bb45e99
KaboomRoads <helmetdemons@gmail.com>
2023-03-11 15:36:49 +0100

    Initial commit.

--- Common/src/main/java/com/kaboomroads/lostfeatures/block/custom/BadlandsCactusBlock.java
+++
@@ -1,9 +0,0 @@
-package com.kaboomroads.lostfeatures.block.custom;
-
-import net.minecraft.world.level.block.CactusBlock;
-
-public class BadlandsCactusBlock extends CactusBlock {
-    protected BadlandsCactusBlock(Properties properties) {
-        super(properties);
-    }
-}
