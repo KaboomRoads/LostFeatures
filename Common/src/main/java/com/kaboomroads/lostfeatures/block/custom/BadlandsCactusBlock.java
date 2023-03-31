fc3e26160512400c391c5eb61296491fc476aead
KaboomRoads <helmetdemons@gmail.com>
2023-03-31 18:11:23 +0200

    Cactus.

--- Common/src/main/java/com/kaboomroads/lostfeatures/block/custom/BadlandsCactusBlock.java
+++ Common/src/main/java/com/kaboomroads/lostfeatures/block/custom/BadlandsCactusBlock.java
@@ -17,15 +17,20 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.pathfinder.PathComputationType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class BadlandsCactusBlock extends PipeBlock {
+    public static final IntegerProperty AGE = BlockStateProperties.AGE_15;
+
     public BadlandsCactusBlock(Properties properties) {
         super(0.25F, properties);
-        registerDefaultState(stateDefinition.any().setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false).setValue(UP, false).setValue(DOWN, false));
+        registerDefaultState(stateDefinition.any().setValue(AGE, 0).setValue(NORTH, false).setValue(EAST, false).setValue(SOUTH, false).setValue(WEST, false).setValue(UP, false).setValue(DOWN, false));
     }
 
     @Override
@@ -41,12 +46,12 @@ public class BadlandsCactusBlock extends PipeBlock {
         BlockState south = blockGetter.getBlockState(blockPos.south());
         BlockState west = blockGetter.getBlockState(blockPos.west());
         return defaultBlockState()
-                .setValue(DOWN, below.is(this) || below.is(ModBlocks.BADLANDS_CACTUS.get()) || below.is(Blocks.SAND) || below.is(Blocks.RED_SAND))
-                .setValue(UP, above.is(this) || above.is(ModBlocks.BADLANDS_CACTUS.get()))
-                .setValue(NORTH, north.is(this) || north.is(ModBlocks.BADLANDS_CACTUS.get()))
-                .setValue(EAST, east.is(this) || east.is(ModBlocks.BADLANDS_CACTUS.get()))
-                .setValue(SOUTH, south.is(this) || south.is(ModBlocks.BADLANDS_CACTUS.get()))
-                .setValue(WEST, west.is(this) || west.is(ModBlocks.BADLANDS_CACTUS.get()));
+                .setValue(DOWN, below.is(this) || below.is(Blocks.SAND) || below.is(Blocks.RED_SAND))
+                .setValue(UP, above.is(this))
+                .setValue(NORTH, north.is(this))
+                .setValue(EAST, east.is(this))
+                .setValue(SOUTH, south.is(this))
+                .setValue(WEST, west.is(this));
     }
 
     @Override
@@ -57,13 +62,8 @@ public class BadlandsCactusBlock extends PipeBlock {
     @NotNull
     @Override
     public BlockState updateShape(BlockState blockState, @NotNull Direction direction, @NotNull BlockState blockState1, @NotNull LevelAccessor levelAccessor, @NotNull BlockPos blockPos, @NotNull BlockPos blockPos1) {
-        if (!blockState.canSurvive(levelAccessor, blockPos)) {
-            levelAccessor.scheduleTick(blockPos, this, 1);
-            return super.updateShape(blockState, direction, blockState1, levelAccessor, blockPos, blockPos1);
-        } else {
-            boolean $$6 = blockState1.is(this) || blockState1.is(ModBlocks.BADLANDS_CACTUS.get()) || direction == Direction.DOWN && (blockState1.is(Blocks.SAND) || blockState1.is(Blocks.RED_SAND));
-            return blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), $$6);
-        }
+        boolean $$6 = blockState1.is(this) || blockState1.is(ModBlocks.BADLANDS_CACTUS.get()) || direction == Direction.DOWN && (blockState1.is(Blocks.SAND) || blockState1.is(Blocks.RED_SAND));
+        return blockState.setValue(PROPERTY_BY_DIRECTION.get(direction), $$6);
     }
 
     @Override
@@ -71,7 +71,6 @@ public class BadlandsCactusBlock extends PipeBlock {
         if (!blockState.canSurvive(level, blockPos)) level.destroyBlock(blockPos, true);
     }
 
-    //TODO: PREVENT FLOATY BITS
     @Override
     public boolean canSurvive(@NotNull BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
         BlockState belowState = levelReader.getBlockState(blockPos.below());
@@ -85,11 +84,46 @@ public class BadlandsCactusBlock extends PipeBlock {
     @Override
     protected void createBlockStateDefinition(StateDefinition.@NotNull Builder<Block, BlockState> builder) {
         super.createBlockStateDefinition(builder);
-        builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN);
+        builder.add(AGE, NORTH, EAST, SOUTH, WEST, UP, DOWN);
     }
 
     @Override
     public boolean isPathfindable(@NotNull BlockState blockState, @NotNull BlockGetter blockGetter, @NotNull BlockPos blockPos, @NotNull PathComputationType type) {
         return false;
+    }
+
+    protected boolean canPlace(@NotNull ServerLevel level, @NotNull BlockPos blockPos) {
+        int i = 0;
+        for (Direction dir : Direction.values()) {
+            BlockPos rel = blockPos.relative(dir);
+            BlockState relState = level.getBlockState(rel);
+            if (dir == Direction.DOWN && (relState.is(Blocks.SAND) || relState.is(Blocks.RED_SAND)) || (relState.is(this) && ++i >= 2))
+                return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void randomTick(@NotNull BlockState blockState, @NotNull ServerLevel level, @NotNull BlockPos blockPos, @NotNull RandomSource random) {
+        ArrayList<BlockPos> possibilities = new ArrayList<>(5);
+        for (Direction dir : Direction.values()) {
+            BlockPos rel = blockPos.relative(dir);
+            if (dir != Direction.DOWN && canPlace(level, rel)) possibilities.add(rel);
+        }
+        if (possibilities.isEmpty()) return;
+        BlockPos relative = possibilities.get(random.nextInt(possibilities.size()));
+        if (level.isEmptyBlock(relative)) {
+            int i;
+            for (i = 1; level.getBlockState(blockPos.below(i)).is(this); ++i) ;
+            if (i < 3) {
+                int j = blockState.getValue(AGE);
+                if (j == 15) {
+                    level.setBlockAndUpdate(relative, getStateForPlacement(level, relative));
+                    BlockState newState = getStateForPlacement(level, blockPos).setValue(AGE, 0);
+                    level.setBlock(blockPos, newState, 3);
+                    level.neighborChanged(newState, relative, this, blockPos, false);
+                } else level.setBlock(blockPos, blockState.setValue(AGE, j + 1), 4);
+            }
+        }
     }
 }
