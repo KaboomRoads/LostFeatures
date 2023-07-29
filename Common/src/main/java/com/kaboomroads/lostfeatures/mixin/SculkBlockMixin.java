e6fd517befcc40cffbf97855d4c8222a2fe9c6a4
KaboomRoads <helmetdemons@gmail.com>
2023-07-29 16:50:24 +0200

    Fixed mixin incompatibility issues.

--- Common/src/main/java/com/kaboomroads/lostfeatures/mixin/SculkBlockMixin.java
+++ Common/src/main/java/com/kaboomroads/lostfeatures/mixin/SculkBlockMixin.java
@@ -2,42 +2,44 @@ package com.kaboomroads.lostfeatures.mixin;
 
 import com.kaboomroads.lostfeatures.block.ModBlocks;
 import com.kaboomroads.lostfeatures.block.custom.SculkJawBlock;
-import com.mojang.datafixers.util.Pair;
+import com.kaboomroads.lostfeatures.utils.SculkPlacement;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.LevelAccessor;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SculkBlock;
 import net.minecraft.world.level.block.SculkShriekerBlock;
+import net.minecraft.world.level.block.SculkSpreader;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(SculkBlock.class)
 public abstract class SculkBlockMixin {
-    private Pair<BlockState, BlockPos> pair;
-
-    @Redirect(method = "attemptUseCharge", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/SculkBlock;getRandomGrowthState(Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/util/RandomSource;Z)Lnet/minecraft/world/level/block/state/BlockState;"))
-    public BlockState redirectGRGS(SculkBlock instance, LevelAccessor $$0, BlockPos $$1, RandomSource $$2, boolean $$3) {
-        pair = getRandomGrowthState($$0, $$1, $$2, $$3);
-        return pair.getFirst();
+    @Inject(method = "attemptUseCharge", at = @At(value = "INVOKE_ASSIGN", target = "Lnet/minecraft/world/level/block/SculkBlock;getRandomGrowthState(Lnet/minecraft/world/level/LevelAccessor;Lnet/minecraft/core/BlockPos;Lnet/minecraft/util/RandomSource;Z)Lnet/minecraft/world/level/block/state/BlockState;"))
+    public void setNewGrowthState(SculkSpreader.ChargeCursor cursor, LevelAccessor level, BlockPos pos, RandomSource random, SculkSpreader spreader, boolean isWorldGen, CallbackInfoReturnable<Integer> cir, @Share("placement") LocalRef<SculkPlacement> placementRef, @Local(ordinal = 2) LocalRef<BlockPos> posRef, @Local(ordinal = 0) LocalRef<BlockState> stateRef) {
+        SculkPlacement placement = lostFeatures$getRandomGrowthState(level, posRef.get(), random, isWorldGen);
+        placementRef.set(placement);
+        stateRef.set(placement.state());
+        posRef.set(placement.pos());
     }
 
-    @Redirect(method = "attemptUseCharge", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/LevelAccessor;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z"))
-    public boolean redirectSB(LevelAccessor instance, BlockPos blockPos, BlockState blockState, int i) {
-        return instance.setBlock(pair.getSecond(), blockState, i);
+    @ModifyExpressionValue(method = "canPlaceGrowth", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 2))
+    private static boolean modifyCanPlaceGrowth(boolean original, @Local(ordinal = 1) BlockState state) {
+        return original || state.is(ModBlocks.SCULK_JAW.get());
     }
 
-    @Redirect(method = "canPlaceGrowth", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/block/state/BlockState;is(Lnet/minecraft/world/level/block/Block;)Z", ordinal = 2))
-    private static boolean redirectCPG(BlockState instance, Block block) {
-        return instance.is(ModBlocks.SCULK_JAW.get()) && instance.is(block);
-    }
-
-    public Pair<BlockState, BlockPos> getRandomGrowthState(LevelAccessor levelAccessor, BlockPos blockPos, RandomSource random, boolean isWorldGen) {
+    @Unique
+    public SculkPlacement lostFeatures$getRandomGrowthState(LevelAccessor levelAccessor, BlockPos blockPos, RandomSource random, boolean isWorldGen) {
         BlockState blockState;
         switch (random.nextInt(14)) {
             case 0 -> blockState = Blocks.SCULK_SHRIEKER.defaultBlockState().setValue(SculkShriekerBlock.CAN_SUMMON, isWorldGen);
@@ -47,6 +49,6 @@ public abstract class SculkBlockMixin {
             }
             default -> blockState = Blocks.SCULK_SENSOR.defaultBlockState();
         }
-        return new Pair<>(blockState.hasProperty(BlockStateProperties.WATERLOGGED) && !levelAccessor.getFluidState(blockPos).isEmpty() ? blockState.setValue(BlockStateProperties.WATERLOGGED, true) : blockState, blockPos);
+        return new SculkPlacement(blockPos, blockState.hasProperty(BlockStateProperties.WATERLOGGED) && !levelAccessor.getFluidState(blockPos).isEmpty() ? blockState.setValue(BlockStateProperties.WATERLOGGED, true) : blockState);
     }
 }
---
+++ Common/src/main/java/com/kaboomroads/lostfeatures/utils/SculkPlacement.java
@@ -0,0 +1,7 @@
+package com.kaboomroads.lostfeatures.utils;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+
+public record SculkPlacement(BlockPos pos, BlockState state) {
+}
